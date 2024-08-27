1694c224891155eecb4d6f0526bd4eede2c39155
yashraj-shitole <yashrajs927@gmail.com>
2024-08-27 15:01:12 +0530

    Question 7 added

---
+++ Question7.java
@@ -0,0 +1,7 @@
+//  Write a program to accept a number from user using command line argument and display its table.
+
+public class Question7 {
+    public static void main(String[] args) {
+        
+    }
+}
